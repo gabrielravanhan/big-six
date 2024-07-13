1bb23cb799e9ab81e7229b961cf6e88b5b97c639
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2024-07-12 22:47:08 -0300

    Criação da segunda migração com Flyway

---
+++ src/main/resources/db/migration/V002__Inserir_Valores_Padroes.sql
@@ -0,0 +1,35 @@
+INSERT INTO posicoes
+    VALUES(01, 'Goleiro'),
+          (02, 'Lateral-direito'),
+          (03, 'Lateral-esquerdo'),
+          (04, 'Zagueiro'),
+          (05, 'Volante'),
+          (06, 'Meia-armador'),
+          (07, 'Meia-atacante'),
+          (08, 'Ponta-direito'),
+          (09, 'Ponta-esquerdo'),
+          (10, 'Centroavante');
+
+INSERT INTO clubes
+    VALUES(01, 'Liverpool'),
+          (02, 'Manchester United'),
+          (03, 'Chelsea'),
+          (04, 'Manchester City'),
+          (05, 'Arsenal'),
+          (06, 'Tottenham');
+
+INSERT INTO temporadas
+    VALUES(01, '2010/2011'),
+          (02, '2011/2012'),
+          (03, '2012/2013'),
+          (04, '2013/2014'),
+          (05, '2014/2015'),
+          (06, '2015/2016'),
+          (07, '2016/2017'),
+          (08, '2017/2018'),
+          (09, '2018/2019'),
+          (10, '2019/2020'),
+          (11, '2020/2021'),
+          (12, '2021/2022'),
+          (13, '2022/2023'),
+          (14, '2023/2024');
