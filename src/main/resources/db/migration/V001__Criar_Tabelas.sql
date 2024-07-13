41ef8ef414d7c8780dff2eeea17b60eb6afff929
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2024-07-12 22:22:44 -0300

    Configuração do banco de dados e criação da primeira migração com Flyway

---
+++ src/main/resources/db/migration/V001__Criar_Tabelas.sql
@@ -0,0 +1,53 @@
+CREATE TABLE posicoes(
+    id      BIGINT,
+    nome    VARCHAR(255) UNIQUE NOT NULL,
+
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE clubes(
+    id      BIGINT,
+    nome    VARCHAR(255) UNIQUE NOT NULL,
+
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE temporadas(
+    id      BIGINT,
+    periodo VARCHAR(009) UNIQUE NOT NULL,
+
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE jogadores(
+    id              BIGINT,
+    nome            VARCHAR(255) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    clube_id        BIGINT NOT NULL,
+
+    PRIMARY KEY(id),
+    FOREIGN KEY(clube_id) REFERENCES clubes(id)
+);
+
+CREATE TABLE posicoes_jogadores(
+    id                  BIGINT,
+    posicao_id          BIGINT NOT NULL,
+    jogador_id          BIGINT NOT NULL,
+    numero_jogos        BIGINT NOT NULL,
+    numero_gols         BIGINT NOT NULL,
+    numero_assistencias BIGINT NOT null,
+
+    PRIMARY KEY(id),
+    FOREIGN KEY(posicao_id) REFERENCES posicoes(id),
+    FOREIGN KEY(jogador_id) REFERENCES jogadores(id)
+);
+
+CREATE TABLE estatisticas(
+    id              BIGINT,
+    temporada_id    BIGINT NOT NULL,
+    jogador_id      BIGINT NOT NULL,
+
+    PRIMARY KEY(id),
+    FOREIGN KEY(temporada_id) REFERENCES temporadas(id),
+    FOREIGN KEY(jogador_id) REFERENCES jogadores(id)
+);
